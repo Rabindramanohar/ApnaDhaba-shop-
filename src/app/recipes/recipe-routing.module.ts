fdbc0da996c90c7259a2df5359bf3c35332e1b0e
Rabindra Mahato <robinmanohar@gmail.com>
2020-01-18 05:44:28 +0530

    stuck in buiding the code

--- src/app/recipes/recipe-routing.module.ts
+++ src/app/recipes/recipe-routing.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from '../app-routing.module';
 
 const routes: Routes = [
     {
-        path: 'recipes',
+        path: '',
         component: RecipesComponent,
         canActivate: [AuthGuard],
         children: [
